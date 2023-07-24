8b3c7698d7bd649414b0f92f05174a7688f31900
taoluo <taoluo@caih.com>
2023-07-24 17:09:22 +0800

    init：初始

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import './self.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='my-wrap'>
+        <div className='my-img'>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
